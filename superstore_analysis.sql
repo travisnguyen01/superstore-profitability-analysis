d016e57c1ba1d6a2b431a6bfe81b8bcc5b0c0e44
travisnguyen01 <travis.nguyen.work01@gmail.com>
2026-03-17 19:58:10 -0700

    superstore_analysis.sql

--- superstore_analysis.sql
+++ superstore_analysis.sql
@@ -4,28 +4,35 @@
 SELECT Discount, Sales, Profit, ROUND(Profit * 1.0 / Sales, 2) AS ProfitMargin
 FROM superstore;
 
-2. Regional Performance
+2. Discount and Sub-Categories
+  Goal: Identify the relationship between average discount levels and sub-category margins
+SELECT "Sub-Category", ROUND(AVG(Discount), 2) AS AvgDiscount, SUM(Sales) AS Sales, SUM(Profit) AS Profit, ROUND(SUM(Profit) * 1.0 / SUM(Sales), 2) AS ProfitMargin
+FROM superstore
+GROUP BY "Sub-Category"
+ORDER BY ProfitMargin;
+
+3. Regional Performance
   Goal: Identify regional trends
 SELECT Region, substr("Order Date", -4) AS year, ROUND(SUM(Sales),1) AS Sales, ROUND(SUM(Profit),1) AS Profit, SUM(Profit) * 1.0 / SUM(Sales) AS ProfitMargin
 FROM superstore
 GROUP BY Region, year
 ORDER BY year, Region;
 
-3. Segment Profitability
+4. Segment Profitability
   Goal: Identify which Segments are driving the most profit
 SELECT Segment, Region, SUM(Profit) AS Profit, ROUND(SUM(Profit) * 100.0 / SUM(Sales), 2) AS ProfitMargin
 FROM superstore
 GROUP BY Segment, Region;
 
 
-4. Subcategory Profit
+5. Subcategory Profit
   Goal: Identify which Sub-Categories are losing and driving profit
 SELECT "Sub-Category", SUM(Profit) AS Profit
 FROM superstore
 GROUP BY "Sub-Category"
 ORDER BY Profit DESC;
 
-5. Subcategory Margins
+6. Subcategory Margins
   Goal: Identify profit margins by Sub-Category
 SELECT Category, "Sub-Category", SUM(Sales) AS Sales, SUM(Profit) AS Profit, ROUND(SUM(Profit) * 100.0 / SUM(Sales), 2) AS ProfitMargin
 FROM superstore
@@ -33,7 +40,7 @@ GROUP BY Category, "Sub-Category"
 ORDER BY ProfitMargin;
 
 
-6. Revenue by Subcategory
+7. Revenue by Subcategory
   Goal: Identify if high revenue correlates with high profit
 SELECT "Sub-Category", SUM(Sales) AS Sales, SUM(Profit) AS Profit, ROUND(SUM(Profit) * 100.0 / SUM(Sales), 2) AS ProfitMargin
 FROM superstore
